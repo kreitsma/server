3d213ddaac99857519ecca7da26376d4cbfacfe9
Mitch Stark <mitchstark@Mitchs-MacBook-Pro-2.local>
2016-11-16 13:59:51 -0500

    include express in dependencies

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
 //This will be the entry point of the server.
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000!');
+});
