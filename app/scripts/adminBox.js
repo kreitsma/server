cefb185e022be54e3fb90ab73a718affa2d32fa0
Mitch Stark <mitchstark@Mitchs-MacBook-Pro-2.local>
2016-11-27 14:12:24 -0500

    build the basic react classes required

--- app/scripts/adminBox.js
+++ app/scripts/adminBox.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
 import $ from 'jquery';
+import JobList from './adminVolunteerJobList.js'
 
 module.exports = React.createClass({
   getInitialState: function(){
     return {data: []};
   },
   componentDidMount: function() {
+    var temp = [{title: "Testing", id: 1, description: "This is a test job."}, {title: "Testing 2 Components", id: 2, description: "Second test job."}]
+    this.setState({data: temp})
   },
   render: function() {
     return (
       <div className="adminBox">
         <h1>Volunteering Jobs</h1>
+        <JobList data={this.state.data} />
       </div>
     );
   }
