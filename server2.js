0828fe38d1c4105a28945ab83030eac897f4304e
Mitch Stark <mitchstark@Mitchs-MacBook-Pro-2.local>
2016-12-05 12:29:12 -0500

    remove second (unused) server file

--- server2.js
+++
@@ -1,9 +0,0 @@
-//This will be the entry point of the server.
-const express = require('express');
-const app = express();
-
-app.listen(3000, function() {
-	console.log('Listening on port 3000!');
-});
-
-app.use(express.static('public'));
